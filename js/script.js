754021010ac4bd4107b62ff20f490618dd303899
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-03-02 13:54:43 +0000

    added script tag

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+  function myButtonClicked() {
+    alert("Hello,World!")
+  }
