a637e5c2149d51dd8537b742605c4df97ab0ddc1
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-18 16:42:34 +0000

    added stuff

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Lamees Hemdan All rights reserved
 //
 // Created by: Lamees Hemdan
-// Created on: March 2023
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-lamees-hemdan/sw.js", {
+    scope: "/ICS2O-Unit-1-08-lamees-hemdan/",
+  })
+}
 
   function myButtonClicked() {
     alert("Hello,World!")
   }
+  
